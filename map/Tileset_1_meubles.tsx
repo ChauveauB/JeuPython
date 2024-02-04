aa998417b4455ff19d330d9d4554cbb6026f545d
ChauveauB <155547704+ChauveauB@users.noreply.github.com>
2024-02-04 16:32:46 +0100

    Nouvelles tilset

---
+++ map/Tileset_1_meubles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.10.2" name="Meubles" tilewidth="25" tileheight="25" tilecount="35" columns="7">
+ <image source="../batiments/2nd/etage_1/tileset/tileset_png/montés/Tileset_1_meubles.png" width="175" height="125"/>
+</tileset>
