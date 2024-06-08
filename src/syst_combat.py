61fbc616e57412725b6bd8b2f7d7fe19a4f9ddca
eszpimporte <eszpimporte@gmail.com>
2024-06-08 13:57:28 +0200

    réarangement

--- src/syst_combat.py
+++ src/syst_combat.py
@@ -7,6 +7,13 @@ class Combat:
     nb_ennemies : int
     fighting = False
 
+    @classmethod
+    def start(cls, player:Player, ennemies:list[Entity]):
+        Combat.fighting = True
+        Combat.player = player
+        Combat.ennemies = ennemies
+        Combat.nb_ennemies = len(Combat.ennemies)
+
     @staticmethod
     def display(screen : pygame.Surface):
         h = screen.get_rect().h
@@ -14,21 +21,18 @@ class Combat:
         h_ent = h/500 * 32
         w_ent = w/800 * 32
 
-        #Mettre le fond de combat
+        #Mettre le fond de combat: >>>>juste faire en sorte que les tailles des blit soient adptables<<<<<
         fond = pygame.image.load("../others/fond_blanc.jpg")
         screen.blit(fond, (0,0))
 
+        Combat.load_ent(screen, w, h, w_ent, h_ent)
+        
+    @staticmethod
+    def load_ent(screen, w, h, w_ent, h_ent):
         #Charger les différents personnages en combat
         screen.blit(Combat.player.images["right"][0], (w*1/6, h*1/3))
         for index in range(Combat.nb_ennemies):
             screen.blit(Combat.ennemies[index].images["left"][0], (w-w_ent-20 if index%2==0 else w-2*w_ent-30, (index+1)*2/15*h))
-
-    @classmethod
-    def start(cls, player:Player, ennemies:list[Entity]):
-        Combat.fighting = True
-        Combat.player = player
-        Combat.ennemies = ennemies
-        Combat.nb_ennemies = len(Combat.ennemies)
 
     @classmethod
     def end():
