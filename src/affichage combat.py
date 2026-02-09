a0f40ed3d22d7b63cabe7164015f29bbaff03601
max <maximus.mendez@gmail.com>
2026-02-09 15:36:02 +0100

    ajout des lifebar et du personnage sur l'ecran de combat

--- src/affichage combat.py
+++ src/affichage combat.py
@@ -1,4 +1,5 @@
 from syst_combat import Personnage
+from animation import AnimateSprite
 import pygame
 
 class combatscreen :
@@ -10,25 +11,54 @@ class combatscreen :
         self.inv_button_img = pygame.image.load('../image_combat/inventory_button.png')
         self.mgc_button_img = pygame.image.load('../image_combat/magic_button.png')
 
-        self.my_lifebar_imb = pygame.image.load(('../image_combat/'))
+        self.player_lifebar_img = pygame.image.load('../image_combat/vie_player.png')
+        self.ennemy_lifebar_img = pygame.image.load('../image_combat/vie_ennemi.png')
+
+
+        #on s'occupe de l'affichage du personnage
+        self.animate_sprite = AnimateSprite("player")
+        self.player_img = self.animate_sprite.get_image(0, 64)
+        on_screen_player = (self.player_img.get_width() * 5, self.player_img.get_height() * 5)
+        self.player_img = pygame.transform.scale(self.player_img, on_screen_player)
+        self.player_img.set_colorkey((0, 0, 0))
+
+
+        #on definit la taille des barres de vie pour qu'elles ne soient pas trop grosses
+        on_screen_lifebar_size = (int(self.player_lifebar_img.get_width() * 0.4), int(self.player_lifebar_img.get_height()*0.4))
+        self.player_lifebar_img = pygame.transform.scale(self.player_lifebar_img, on_screen_lifebar_size)
+        self.ennemy_lifebar_img = pygame.transform.scale(self.ennemy_lifebar_img, on_screen_lifebar_size)
+
 
         #on definit les positions de chaque boutton d'action
         self.atk_button_pos = (850, 600)
         self.inv_button_pos = (100, 600)
         self.mgc_button_pos = (1050, 600)
 
+
+        #on definit les positions des barres de vie
+        self.player_lifebar_pos = (100, 50)
+        self.ennemy_lifebar_pos = (800, 50)
+
+        self.player_pos = (200, 300)
+
+
     def draw_fight_screen (self) :
         #on rempli le fond pour cacher la carte
         self.screen.fill((50, 50, 50))
 
-
+        #on dessine les elements de l'ecran
         self.screen.blit(self.atk_button_img, self.atk_button_pos)
         self.screen.blit(self.inv_button_img, self.inv_button_pos)
         self.screen.blit(self.mgc_button_img, self.mgc_button_pos)
 
-#ce qui suit n'est la que pour tester, il faudra le supprimer pour l'inclure correctement dasn le jeu
+        self.screen.blit(self.player_lifebar_img, self.player_lifebar_pos)
+        self.screen.blit(self.ennemy_lifebar_img, self.ennemy_lifebar_pos)
+
+        self.screen.blit(self.player_img, self.player_pos)
+
+#ce qui suit n'est la que pour tester, il faudra le supprimer pour l'inclure correctement dans le jeu
 pygame.init()
-screen = pygame.display.set_mode((1080, 720), pygame.FULLSCREEN) # Crée la fenêtre
+screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN) # Crée la fenêtre
 ui = combatscreen(screen)
 
 while True: # Boucle infinie simple
